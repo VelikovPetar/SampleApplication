52f025092eec3da06d90d9251e8bf1eb4e558aaf
velik_000 <petarvelikov95@gmail.com>
2017-02-16 19:46:47 +0100

    Fixed tags

--- app/src/main/java/com/example/velik_000/sampleapplication/CallAndSMSBroadcastReceiver.java
+++ app/src/main/java/com/example/velik_000/sampleapplication/CallAndSMSBroadcastReceiver.java
@@ -16,6 +16,7 @@ public class CallAndSMSBroadcastReceiver extends BroadcastReceiver {
     private final String ACTION_PHONE = "android.intent.action.PHONE_STATE";
     private final String ACTION_SMS = "android.provider.Telephony.SMS_RECEIVED";
     final SmsManager sms = SmsManager.getDefault();
+    private String TAG = "IncBCST";
 
     public CallAndSMSBroadcastReceiver() {
 
@@ -23,51 +24,43 @@ public class CallAndSMSBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("IncMsg", "BROADCAST RECEIVED");
+        Log.d(TAG, "BROADCAST RECEIVED");
         this.context = context;
         String action = intent.getAction();
         if(action.equals(ACTION_PHONE)) {  // Ako e primen broadcast od PHONE
-            CustomPhoneStateListener cpsl = new CustomPhoneStateListener();
             TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
             // Proveri ja sostojbata na povikot
-            tm.listen(cpsl, PhoneStateListener.LISTEN_CALL_STATE);
-        } else if(action.equals(ACTION_SMS)) {  // Ako e primen broadcast od SMS
-            Log.d("IncMsg", "INSIDE MSG PART");
-            Bundle bundle = intent.getExtras();
-            try {
-                if (bundle != null) {
-                    Log.d("IncMsg", "BUNDLE NOT NULL");
-                    Object[] pdus = (Object[]) bundle.get("pdus");
-                    if (pdus != null) {
-                        Log.d("IncMsg", "PDUS NOT NULL");
-                        for (int i = 0; i < pdus.length; ++i) {
-                            Log.d("IncMsg", "IN FOR LOOP");
-                            SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                            String phoneNumber = message.getDisplayOriginatingAddress();
-                            Log.d("IncMsg", "Incoming message from " + phoneNumber);
-                        }
+            tm.listen(new PhoneStateListener(){
+                @Override
+                public void onCallStateChanged(int state, String incomingNumber) {
+                    if(state == TelephonyManager.CALL_STATE_RINGING) {
+                        // TODO Zapishi vo baza
+                        Log.d(TAG, "Incoming call from " + incomingNumber);
+                    } else if(state == TelephonyManager.CALL_STATE_OFFHOOK) {
+                        Log.d(TAG, "OFF_HOOK");
+                    } else if(state == TelephonyManager.CALL_STATE_IDLE) {
+                        Log.d(TAG, "IDLE");
                     }
                 }
-            }catch (Exception e) {
-                Log.e("IncSms", "Exception " + e);
+            }, PhoneStateListener.LISTEN_CALL_STATE);
+        } else if(action.equals(ACTION_SMS)) {  // Ako e primen broadcast od SMS
+            Log.d(TAG, "INSIDE MSG PART");
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Log.d(TAG, "BUNDLE NOT NULL");
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if (pdus != null) {
+                    Log.d(TAG, "PDUS NOT NULL");
+                    for (int i = 0; i < pdus.length; ++i) {
+                        Log.d(TAG, "IN FOR LOOP");
+                        SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                        String phoneNumber = message.getDisplayOriginatingAddress();
+                        Log.d(TAG, "Incoming message from " + phoneNumber);
+                    }
+                    // TODO Zapishi vo baza
+                }
             }
         }
     }
 
-    private class CustomPhoneStateListener extends PhoneStateListener {
-
-        private String TAG = "CustomPSL";
-
-        @Override
-        public void onCallStateChanged(int state, String incomingNumber) {
-            // Spravi se so sostojbata na povikot soodvetno
-            if(state == TelephonyManager.CALL_STATE_RINGING) {
-                Log.d(TAG, "Incoming call from " + incomingNumber);
-            } else if(state == TelephonyManager.CALL_STATE_OFFHOOK) {
-                Log.d(TAG, "OFF_HOOK");
-            } else if(state == TelephonyManager.CALL_STATE_IDLE) {
-                Log.d(TAG, "IDLE");
-            }
-        }
-    }
 }
